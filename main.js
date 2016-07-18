c3365380da0ad9dfce53429fad31765c7c933f87
Nathan Glover <nathan@glovers.id.au>
2016-07-18 19:27:08 +0800

    Fixed profiler

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // import modules
 require('prototype.spawn')();
-var profiler = require('screeps-profiler');
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 //var roleWallRepairer = require('role.wallRepairer');
+
+var profiler = require('screeps-profiler');
 
 profiler.enable();
 module.exports.loop = function () {
